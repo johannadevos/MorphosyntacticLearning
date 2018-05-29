fbaaf84cb967583a083340904861890c1e661309
Johanna de Vos <johannadevos@gmail.com>
2018-05-29 14:17:19 +0200

    First mixed-effects models (fixed variables from research questions, and random intercepts over participants and items).

--- code/Statistical_models.R
+++ code/Statistical_models.R
@@ -6,9 +6,36 @@ rm(list=ls())
 
 # Read in data
 data <- read.csv("../data/Long_data.csv")
-data$X <- NULL
 str(data)
 
 # Remove unaware participants
 data <- data[data$learningtype!="unaware",]
 
+# Models
+fixed <- glm(bin_score ~ learningtype + verbtype + input + testmoment + input:testmoment, family = 'binomial', data = data)
+summary(fixed)
+
+# First mixed-effects model with random intercepts over participants
+mixed1 <- glmer(bin_score ~ input*testmoment*learningtype + input*testmoment*verbtype + (1|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5)))
+summary(mixed)
+
+# Adding random intercepts over items
+mixed2 <- glmer(bin_score ~ input*testmoment*learningtype + input*testmoment*verbtype + (1|participant) + (1|item), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5)))
+
+# Have the random intercepts over items significantly increased model fit?
+anova(mixed, mixed2)
+
+# Now, turn things around
+# First mixed-effects model with random intercepts over items
+mixed3 <- glmer(bin_score ~ input*testmoment*learningtype + input*testmoment*verbtype + (1|item), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5)))
+summary(mixed)
+
+# Adding random intercepts over participants
+mixed4 <- glmer(bin_score ~ input*testmoment*learningtype + input*testmoment*verbtype + (1|item) + (1|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5)))
+summary(mixed4)
+
+# Have the random intercepts over participants significantly increased model fit?
+anova(mixed3, mixed4)
+
+# Lower AIC, BIC and deviance indicates a better fit of the model to the data
+# If Pr(>Chisq) is below .05, the second model fits the data significantly better than the first
