0e5571c46420d830bc8dbf455779f6c4cb74d5d5
Johanna de Vos <johannadevos@gmail.com>
2018-05-29 14:38:40 +0200

    Making use of tictoc library to measure running time.

--- code/Statistical_models.R
+++ code/Statistical_models.R
@@ -1,5 +1,5 @@
 # Libraries
-library(lme4)
+library(lme4); library(tictoc)
 
 # Clear workspace
 rm(list=ls())
@@ -16,22 +16,22 @@ fixed <- glm(bin_score ~ learningtype + verbtype + input + testmoment + input:te
 summary(fixed)
 
 # First mixed-effects model with random intercepts over participants
-mixed1 <- glmer(bin_score ~ input*testmoment*learningtype + input*testmoment*verbtype + (1|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5)))
+tic(); mixed1 <- glmer(bin_score ~ input*testmoment*learningtype + input*testmoment*verbtype + (1|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); toc()
 summary(mixed)
 
 # Adding random intercepts over items
-mixed2 <- glmer(bin_score ~ input*testmoment*learningtype + input*testmoment*verbtype + (1|participant) + (1|item), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5)))
+tic(); mixed2 <- glmer(bin_score ~ input*testmoment*learningtype + input*testmoment*verbtype + (1|participant) + (1|item), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); toc()
 
 # Have the random intercepts over items significantly increased model fit?
 anova(mixed, mixed2)
 
 # Now, turn things around
 # First mixed-effects model with random intercepts over items
-mixed3 <- glmer(bin_score ~ input*testmoment*learningtype + input*testmoment*verbtype + (1|item), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5)))
+tic(); mixed3 <- glmer(bin_score ~ input*testmoment*learningtype + input*testmoment*verbtype + (1|item), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); toc()
 summary(mixed)
 
 # Adding random intercepts over participants
-mixed4 <- glmer(bin_score ~ input*testmoment*learningtype + input*testmoment*verbtype + (1|item) + (1|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5)))
+tic(); mixed4 <- glmer(bin_score ~ input*testmoment*learningtype + input*testmoment*verbtype + (1|item) + (1|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); toc()
 summary(mixed4)
 
 # Have the random intercepts over participants significantly increased model fit?
@@ -39,3 +39,11 @@ anova(mixed3, mixed4)
 
 # Lower AIC, BIC and deviance indicates a better fit of the model to the data
 # If Pr(>Chisq) is below .05, the second model fits the data significantly better than the first
+
+# Does including proficiency in the model lead to a better fit of the model to the data?
+tic(); mixed5 <- glmer(bin_score ~ input*testmoment*learningtype + input*testmoment*verbtype + lextale + (1|item) + (1|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); toc()
+summary(mixed5)
+
+# Have the LexTALE scores significantly increased model fit?
+anova(mixed4, mixed5)
+
