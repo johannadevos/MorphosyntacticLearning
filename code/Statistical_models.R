c42f6992d3d0d6b70ca9d4ce2a0b46bad5881f1c
Johanna de Vos <johannadevos@gmail.com>
2018-06-11 10:39:03 +0200

    Removed random slopes of verbtype over items, removed verbtype*testmoment interaction, added the calculation of more confidence intervals and odds ratios.

--- code/Statistical_models.R
+++ code/Statistical_models.R
@@ -78,31 +78,47 @@ summary(rePCA(verbtype_part)) # All dimensions are supported by the data
 
 # Random slope of verbtype over item
 tic(); verbtype_item <- glmer(bin_score ~ input*testmoment*learningtype*verbtype + (1+verbtype|item) + (1+verbtype|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); toc()
-anova(verbtype_part, verbtype_item) # Model fit increases significantly
-summary(rePCA(verbtype_item)) # All dimensions are supported by the data
+# Output: Warning message 'Model failed to converge'; remove this random slope again from the model
 
 # Random slope of testmoment over participant
-tic(); testmoment_part <- glmer(bin_score ~ input*testmoment*learningtype*verbtype + (1+verbtype|item) + (1+verbtype+testmoment|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); toc()
+tic(); testmoment_part <- glmer(bin_score ~ input*testmoment*learningtype*verbtype + (1|item) + (1+verbtype+testmoment|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); toc()
 anova(verbtype_item, testmoment_part) # No significant improvement, and even a higher AIC
 
 # Random slope of testmoment over item
-tic(); testmoment_item <- glmer(bin_score ~ input*testmoment*learningtype*verbtype + (1+verbtype+testmoment|item) + (1+verbtype|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); toc()
+tic(); testmoment_item <- glmer(bin_score ~ input*testmoment*learningtype*verbtype + (1+testmoment|item) + (1+verbtype|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); toc()
 anova(verbtype_item, testmoment_item) # Marginal improvement
 summary(rePCA(testmoment_item)) # Third dimension over items is not supported by the model
 
-# Final model (= identical to verbtype_item)
-final <- verbtype_item
+# Final model (= identical to verbtype_part)
+final <- verbtype_part
 summary(final)
 
 # Obtain confidence intervals and odds ratios for the final model
-se <- sqrt(diag(vcov(final)))
-(tab <- cbind(Est = fixef(final), LL = fixef(final) - 1.96*se, UL = fixef(final) + 1.96*se)) # CIs
-exp(tab) # Calculate and print odds ratios
+se_final <- sqrt(diag(vcov(final)))
+(tab_final <- cbind(Est = fixef(final), LL = fixef(final) - 1.96*se_final, UL = fixef(final) + 1.96*se_final)) # CIs
+exp(tab_final) # Calculate and print odds ratios
 
 # Final model with 'incidental' as intercept
 data$learningtype <- factor(data$learningtype, levels = c("incidental", "explicit"))
-final_inc <- glmer(bin_score ~ input*testmoment*learningtype*verbtype + (1+verbtype|item) + (1+verbtype|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5)))
+final_inc <- glmer(bin_score ~ input*testmoment*learningtype*verbtype + (1|item) + (1+verbtype|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5)))
 summary(final_inc)
+
+# Obtain confidence intervals and odds ratios for the final_inc model
+se_final_inc <- sqrt(diag(vcov(final_inc)))
+(tab_final_inc <- cbind(Est = fixef(final_inc), LL = fixef(final_inc) - 1.96*se_final_inc, UL = fixef(final_inc) + 1.96*se_final_inc)) # CIs
+exp(tab_final_inc) # Calculate and print odds ratios
+
+# Final model with T3 as intercept (to investigate the main effect of condition at T3, the explicit posttest)
+# (while input = no input, verbtype = critical, learningtype = explicit)
+data$testmoment <- factor(data$testmoment, levels = c("T3", "T2", "T1")) # Relevel testmoment
+data$learningtype <- factor(data$learningtype, levels = c("explicit", "incidental")) # Relevel learningtype
+final_T3 <- glmer(bin_score ~ input*testmoment*learningtype*verbtype + (1|item) + (1+verbtype|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5)))
+summary(final_T3)
+
+# Obtain confidence intervals and odds ratios for the final_T3 model
+se_final_T3 <- sqrt(diag(vcov(final_T3)))
+(tab_final_T3 <- cbind(Est = fixef(final_T3), LL = fixef(final_T3) - 1.96*se_final_T3, UL = fixef(final_T3) + 1.96*se_final_T3)) # CIs
+exp(tab_final_T3) # Calculate and print odds ratios
 
 
 ## Investigate model fit
@@ -119,15 +135,20 @@ binnedplot(fitted(final), resid(final, type = "response"), cex.pts=1, col.int="b
 data$testmoment <- factor(data$testmoment, levels = c("T1", "T2", "T3"))
 
 # Model
-tic(); simple <- glmer(bin_score ~ verbtype*learningtype + testmoment*learningtype + (1+verbtype|item) + (1+verbtype|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); toc()
+tic(); simple <- glmer(bin_score ~ verbtype*learningtype + testmoment*learningtype + (1|item) + (1+verbtype|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); toc()
 summary(simple)
 
 # Obtain confidence intervals and odds ratios for the simple model
-se_main <- sqrt(diag(vcov(simple)))
-(tab_main <- cbind(Est = fixef(simple), LL = fixef(simple) - 1.96*se_main, UL = fixef(simple) + 1.96*se_main)) # CIs
-exp(tab_main) # Calculate and print odds ratios
+se_simple <- sqrt(diag(vcov(simple)))
+(tab_simple <- cbind(Est = fixef(simple), LL = fixef(simple) - 1.96*se_simple, UL = fixef(simple) + 1.96*se_simple)) # CIs
+exp(tab_simple) # Calculate and print odds ratios
 
 # Relevel to obtain insight in control items
 data$verbtype <- factor(data$verbtype, levels = c("control", "critical"))
-tic(); simple_contr <- glmer(bin_score ~ verbtype*learningtype + testmoment*learningtype + verbtype*testmoment + (1+verbtype|item) + (1+verbtype|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); toc()
+tic(); simple_contr <- glmer(bin_score ~ verbtype*learningtype + testmoment*learningtype + (1|item) + (1+verbtype|participant), family = 'binomial', data = data, control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); toc()
 summary(simple_contr)
+
+# Obtain confidence intervals and odds ratios for the simple_contr model
+se_simple_contr <- sqrt(diag(vcov(simple_contr)))
+(tab_simple_contr <- cbind(Est = fixef(simple_contr), LL = fixef(simple_contr) - 1.96*se_simple_contr, UL = fixef(simple_contr) + 1.96*se_simple_contr)) # CIs
+exp(tab_simple_contr) # Calculate and print odds ratios
